f34087ab36145f2dbdad64e80bf4412bf9c26725
Daniel Doran <ddoran3@gmail.com>
2017-08-12 21:31:08 -0600

    rename to cbdl

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="cdl",
+	name="cbdl",
 	version="0.0.1",
-	url="https://github.com/GiantBlargg/cdl",
+	url="https://github.com/GiantBlargg/cbdl",
 	author="GiantBlargg",
 	license="MIT",
 	install_requires=["requests","click"],
 	python_requires=">=3",
 	packages=find_packages("src"),
 	package_dir={'':'src'},
-	entry_points={"console_scripts":"cdl = main:main"}
+	entry_points={"console_scripts":"cbdl = main:main"}
 )
