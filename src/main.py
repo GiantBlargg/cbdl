41d4642286e2285fc997fc4133b65228d56f3ae5
Daniel Doran <ddoran3@gmail.com>
2017-08-12 20:29:35 -0600

    added archive-type option; does nothing

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import click
 import re
 
 @click.command()
+@click.option("--archive-type","-t", type=click.Choice(["7","a","r","t","z"]), default="z")
 @click.argument("URL")
 def main(url):
 	m=re.match("^(?:(.*):\/\/)?([a-zA-Z0-9-\.]+\.[a-zA-Z0-9-]+)(?:\/(.*))?$", url)
