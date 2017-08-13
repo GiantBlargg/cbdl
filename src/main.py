ed07809ac8579ada5c0f8f9dbe9112f26cebfd0a
Daniel Doran <ddoran3@gmail.com>
2017-08-12 21:59:43 -0600

    define new options

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ from sites import siteHandle
 
 
 @click.command()
+@click.option("--dest", "-d", type=click.Choice(file_okay=False), default=".")
 @click.option("--archive-type","-t", type=click.Choice(["7","a","r","t","z"]), default="z")
+@click.option("--output", "-o", default="${series}/${volume}/${chapter}-${title}")
 @click.argument("URL")
-def main(url):
+def main(dest, type, output, url):
 	siteHandler(url)
 
